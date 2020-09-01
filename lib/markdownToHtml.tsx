118fd8939ac93f86f4d086cd89af977290ef1538
Resi Respati <resir014@gmail.com>
2020-09-01 23:14:58 +0700

    add support for syntax highlighting

--- lib/markdownToHtml.tsx
+++ lib/markdownToHtml.tsx
@@ -1,7 +1,19 @@
-import remark from 'remark'
-import html from 'remark-html'
+import markdownit from 'markdown-it'
+import * as shiki from 'shiki'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const highlighter = await shiki.getHighlighter({ theme: 'dark-plus' })
+  const md = markdownit({
+    html: true,
+    highlight: (code, lang) => {
+      if (highlighter.codeToHtml) {
+        return highlighter.codeToHtml(code, lang)
+      }
+
+      return code
+    }
+  })
+
+  const result = md.render(markdown)
+  return result
 }
